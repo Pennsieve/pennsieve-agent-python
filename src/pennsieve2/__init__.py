434ffb1f4dcd6a8c1f0297a59d0aa0c9e5a77bd1
athril <patryk.orzechowski@gmail.com>
2022-11-15 18:18:08 -0500

    Pre-release v0.0.5

--- src/pennsieve2/__init__.py
+++ src/pennsieve2/__init__.py
@@ -1,3 +1,14 @@
-from importlib_metadata import version
 from .pennsieve import Pennsieve
-__version__ = version(__package__)
+
+
+try:
+    import importlib.metadata as importlib_metadata
+except ModuleNotFoundError:
+    import importlib_metadata
+
+
+try:
+    # This will read version from pyproject.toml
+    __version__ = importlib_metadata.version(__name__)
+except Exception:
+    __version__ = "unknown"
